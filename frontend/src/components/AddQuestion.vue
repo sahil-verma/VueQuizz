196472743c03f6ce3e6f0bee57d3080ae8217167
sahil-verma <Sahil.Verma2@MyGeorgian.ca>
2018-04-28 21:31:54 -0400

    Added add question component

---
+++ frontend/src/components/AddQuestion.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>Add question form will be displayed here</h1>
+</template>
+
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class AddQuestion extends Vue {
+  
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
+
