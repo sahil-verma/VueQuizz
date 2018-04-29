36528822dc011a5681ee9dbc267ff88123338326
sahil-verma <Sahil.Verma2@MyGeorgian.ca>
2018-04-29 18:15:35 -0400

    Created the database context

--- backend/VueQuizzWebApi/QuizContext.cs
+++ backend/VueQuizzWebApi/QuizContext.cs
@@ -2,11 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-//using Microsoft.Enity;
+using Microsoft.EntityFrameworkCore;
 
 namespace VueQuizzWebApi
 {
-    public class QuizContext 
+    public class QuizContext : DbContext
     {
+
+        public QuizContext(DbContextOptions<QuizContext> options) : base(options) { }
+
+        // dataset for questions
+        public DbSet<Models.Question> QuestionsDb { get; set; }
     }
 }
