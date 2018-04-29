37b5c6e25af07527b6c7cea1bebf80e6ee228475
sahil-verma <Sahil.Verma2@MyGeorgian.ca>
2018-04-28 21:32:09 -0400

    Added the Quiz view

---
+++ frontend/src/views/QuestionsQuiz.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img src="../assets/logo.png">
+    <AddQuestion />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import AddQuestion from '@/components/AddQuestion.vue'; // @ is an alias to /src
+
+@Component({
+  components: {
+    AddQuestion,
+  },
+})
+export default class QuestionsForm extends Vue {}
+</script>
