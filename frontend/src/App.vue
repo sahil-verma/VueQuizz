bb9511b7586233ebf1b82bdfcf0aba6b4a55e552
sahil-verma <Sahil.Verma2@MyGeorgian.ca>
2018-04-28 23:45:47 -0400

    Edited the app layout

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,25 +1,38 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link>
+      <router-link to="/" class="route-link-item">Home</router-link>
     </div>
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+html, body {
+  height: 100%;
 }
+body {
+  font-family: 'Montserrat', sans-serif;
+  background-position: center;
+  background-image: url('/images/backgrounds/home/bench-accounting-49909-unsplash.jpg');
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+
+#question-quiz h1.title {
+  font-size: 3em;
+  font-family: 'Cabin Sketch', 'Montserrat', sans-serif;
+}
+
 #nav {
   padding: 30px;
   a {
     font-weight: bold;
     color: #2c3e50;
+    // border-radius: 100%;
+    // border: 3px solid rgba($color: #000, $alpha: 0.1);
+    // padding: 20px 15px;
+    // background-color: #fff; 
     &.router-link-exact-active {
       color: #42b983;
     }
