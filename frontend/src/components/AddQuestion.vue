d853d172ec1c3f09c8f4ebecfe0852c564a12c4c
sahil-verma <Sahil.Verma2@MyGeorgian.ca>
2018-04-28 23:46:22 -0400

    Completed the AddQuestion component

--- frontend/src/components/AddQuestion.vue
+++ frontend/src/components/AddQuestion.vue
@@ -1,18 +1,59 @@
 <template>
-  <h1>Add question form will be displayed here</h1>
+  <section class="container">
+    <div class="columns">
+      <div class="column is-three-fifths is-block-mobile">
+        <input type="text" class="input" v-model="question.text" placeholder="Enter your question here" style="width:80%">
+      </div>
+      <div class="column is-block-mobile">
+        <button v-on:click.stop="addButtonClick" class="button is-primary"><i class="fas fa-plus"></i>Add to Quiz</button>
+      </div>
+    </div>
+    
+  </section>
 </template>
 
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
+// import Questions API Service handler
+import { QuizApiService } from '../services/quiz.api'
 
 @Component
 export default class AddQuestion extends Vue {
+
+  // declare class properties
+  question: Object;
+  quizApi: QuizApiService;
+
+  constructor() {
+    super();
+    // Instantiate the questions object to store the input text in 'text' field
+    this.question = {
+      text: ""
+    };
+    // initialize an instance of QuizApiService
+    this.quizApi = new QuizApiService;
+  }
+
+  
+
+  // onClick event handler for the AddQuestionButton
+  addButtonClick (): void {
+    console.dir(this.question)
+  }
   
 }
 </script>
 
 <style scoped lang="scss">
-
+section > div > div:first-child {
+  text-align: right;
+}
+section > div > div:nth-child(2) {
+  text-align: left;
+}
+i[class="fas fa-plus"] {
+  padding-right: 10px;
+}
 </style>
 
