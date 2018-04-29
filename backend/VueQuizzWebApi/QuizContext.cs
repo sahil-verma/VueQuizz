2bc945a9fe6e745a6cfd2831f90cc3c9d42908c4
sahil-verma <Sahil.Verma2@MyGeorgian.ca>
2018-04-29 17:44:48 -0400

    Updated the reference to EntityFrameworkCore

---
+++ backend/VueQuizzWebApi/QuizContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+//using Microsoft.Enity;
+
+namespace VueQuizzWebApi
+{
+    public class QuizContext 
+    {
+    }
+}
