cb355cbb5a71b37ce814060f5cf1cd4632fb0792
sahil-verma <Sahil.Verma2@MyGeorgian.ca>
2018-04-29 18:15:48 -0400

    configured the database context

--- backend/VueQuizzWebApi/Startup.cs
+++ backend/VueQuizzWebApi/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
 
 namespace VueQuizzWebApi
 {
@@ -34,6 +35,9 @@ namespace VueQuizzWebApi
                 .AllowAnyMethod()
                 .AllowAnyHeader();
             }));
+
+            // configure DbContext to use an in memory database
+            services.AddDbContext<QuizContext>(options => options.UseInMemoryDatabase("quiz"));
             services.AddMvc();
         }
 
