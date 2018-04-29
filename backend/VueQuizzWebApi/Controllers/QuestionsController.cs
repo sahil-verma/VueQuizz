cfe17f2a52cc13b78ceff40f42785def19d45807
sahil-verma <Sahil.Verma2@MyGeorgian.ca>
2018-04-29 15:49:22 -0400

    setup the api backend

---
+++ backend/VueQuizzWebApi/Controllers/QuestionsController.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VueQuizzWebApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Questions")]
+    public class QuestionsController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public string Get()
+        {
+            return "VueQuizz Web Api";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+    }
+}
